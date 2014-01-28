9212ec7a5b1c01110a34a144f5495a157b8135a2
Apprentice <apprentice@dbc-chicago-73.local>
2014-01-27 19:01:04 -0600

    About to have our mind blown by AJAX. Finished the JQuery UI stuff.

--- public/js/application.js
+++ public/js/application.js
@@ -1,23 +1,9 @@
 $(document).ready(function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
-
-    $( "#sortable" ).mouseup(function(){
-      setTimeout(reorder(),2000)
-      // $( "#sortable > li" ).each(function() {
-      //   var list_items = [];
-      //   list_items.push($( this ).text());
-      //   console.log(list_items)
-      // });
-    });
+    $( "#sortable" ).on( "sortupdate", function( event, ui ) {
+      this.innerText
+    } );
 });
 
-var reorder = function(){
-  var people = document.getElementsByClassName("people");
-  console.log(people)
-  var orderedPeople = [];
-  for (var i = 0; i < people.length; i++) {
-    orderedPeople.push(people[i].innerText);
-  };
-  console.log(orderedPeople);
-};
+
